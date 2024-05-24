4ccf05741168bbf6757554bd932e7ea63e0498bf
VAHC <victor10alha@gmail.com>
2024-05-24 19:22:29 -0300

    update commit

--- front/validacion.js
+++ front/validacion.js
@@ -13,14 +13,14 @@ document.addEventListener("DOMContentLoaded", function () {
         });
 
         // Validación del nombre
-        const nombre = document.getElementById('firstname').value;
+        const nombre = document.getElementById('nombre').value;
         if (nombre.trim() === '') {
             document.getElementById('error-nombre').textContent = 'Por favor ingrese su nombre';
             errores++;
         }
 
         // Validación del apellido
-        const apellido = document.getElementById('lastname').value;
+        const apellido = document.getElementById('apellido').value;
         if (apellido.trim() === '') {
             document.getElementById('error-apellido').textContent = 'Por favor ingrese su apellido';
             errores++;
@@ -47,14 +47,7 @@ document.addEventListener("DOMContentLoaded", function () {
             document.getElementById('error-telefono').textContent = 'Por favor ingrese un número de teléfono válido (10 dígitos)';
             errores++;
         }
-
-        // Validación de la sección de interés
-        const interes = document.getElementById('interes').value;
-        if (interes === 'default') {
-            document.getElementById('error-interes').textContent = 'Por favor seleccione una sección de interés';
-            errores++;
-        }
-
+        
         // Validación de los términos y condiciones
         const acepto = document.getElementById('acepto').checked;
         if (!acepto) {
@@ -72,3 +65,89 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+// // Seleccionar el formulario por su ID
+// const formulario = document.querySelector("formulario");
+
+// // Función para limpiar las validaciones anteriores
+// const clearValidations = () => {
+//     // Seleccionar todos los campos de entrada y elementos de error
+//     const inputs = document.querySelectorAll("input");
+//     const errorElements = document.querySelectorAll(".error");
+
+//     // Iterar sobre los campos de entrada y elementos de error y limpiarlos
+//     inputs.forEach(input => {
+//         input.classList.remove('error');
+//         // Verificar si el elemento hermano siguiente existe antes de acceder a su propiedad textContent
+//         // No todos los inputs tienen a continuacion un div class error.
+//         const nextSibling = input.nextElementSibling;
+//         if (nextSibling !== null && nextSibling.classList.contains('error')) {
+//             nextSibling.textContent = '';
+//         }
+//     });
+
+//     errorElements.forEach(errorElement => {
+//         errorElement.textContent = '';
+//     });
+// }
+
+// // Función para validar el formulario
+// const validarFormulario = (event) => {
+//     // Prevenir el comportamiento predeterminado de envío del formulario
+//     event.preventDefault();
+//     // Limpiar las validaciones anteriores
+//     clearValidations();
+
+//     // Obtener referencias a los campos del formulario
+//     const firstname = document.querySelector("#nombre");
+//     const lastname = document.querySelector("#apellido");
+//     const email = document.querySelector("#email");
+//     const telefono = document.querySelector("#telefono");
+//     const acepto = document.querySelector("#acepto");
+
+//     let validation = true;
+//     // Validar el campo firstname
+//     if(!nombre.value.trim()){
+//         // alert("Los campos Nombre, Apellido, Email, Telefono, Términos son obligatorios")
+//         firstname.classList.add('error')
+//         document.querySelector("#error-nombre").textContent='Debe completar el campo Nombre';
+//         validation=false;
+//     }
+//     // Validar el campo lastname
+//     if(!apellido.value.trim()){
+//         // alert("Los campos Nombre, Apellido, Email, Telefono, Términos son obligatorios")
+//         document.querySelector("#error-apellido").textContent='Debe completar el campo Apellido';
+//         lastname.classList.add('error')
+//         validation=false;
+//     }
+//     // Validar el campo email
+//     if(!email.value.trim()){
+//         // alert("Los campos Nombre, Apellido, Email, Telefono, Términos son obligatorios")
+//         document.querySelector("#error-email").textContent='Debe completar el campo Email';
+//         email.classList.add('error')
+//         validation=false;
+//     }
+//     // Validar telefono
+//     if(!telefono.value.trim()){
+//         // alert("Los campos Nombre, Apellido, Email, Telefono, Términos son obligatorios")
+//         document.querySelector("#error-telefono").textContent='Debe completar el campo Telefono';
+//         telefono.classList.add('error')
+//         validation=false;
+//     }
+//     // Validar de términos y condiciones
+//     if(!acepto.checked){
+//         // alert("Los campos Nombre, Apellido, Email, Telefono, Términos on obligatorios")
+//         document.querySelector("#error-acepto").textContent='Debe aceptar los términos y condiciones';
+//         acepto.classList.add('error')
+//         validation=false;
+//     }
+//     // Si no hay errores de validación, enviar el formulario
+//     if(validation){
+//         formulario.submit()
+//     }
+// }
+
+// formulario.addEventListener('submit',validarFormulario);
+// });
